18ce47d5c433ec005a82fc72dd2551ee17041588
Jonathan Bai <gamwang@berkeley.edu>
2015-10-19 20:49:08 -0700

    Finished hw1

--- Hangman/HangmanViewController.swift
+++ Hangman/HangmanViewController.swift
@@ -10,6 +10,11 @@ import UIKit
 
 class HangmanViewController: UIViewController {
 
+    @IBOutlet weak var newGameButton: UIButton!
+    @IBOutlet weak var hangmanImage: UIImageView!
+    @IBOutlet weak var guessButton: UIButton!
+    @IBOutlet weak var currentLetters: UILabel!
+    @IBOutlet weak var inputText: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
